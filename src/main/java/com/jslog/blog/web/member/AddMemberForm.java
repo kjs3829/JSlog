8faa047f71d8af52e9fea952d40ccbb6082751f8
JunSeong <kjs35323@naver.com>
2021-08-03 22:16:24 +0900

    feat: 회원가입 기능 개발

---
+++ src/main/java/com/jslog/blog/web/member/AddMemberForm.java
@@ -0,0 +1,19 @@
+package com.jslog.blog.web.member;
+
+import lombok.Data;
+
+@Data
+public class AddMemberForm {
+    private String email;
+    private String password;
+    private String nickname;
+
+    public AddMemberForm() {
+    }
+
+    public AddMemberForm(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
