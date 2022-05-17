42a48b9556069a19592f4ab7db1e69feb4abb84b
JunSeong <kjs35323@naver.com>
2022-05-17 18:16:19 +0900

    test: 직접작성한 페이징검증코드 대신 assertj의 contain메서드로 변경
    +setUp 대신 각 테스트에서 사전 환경을 구성하게 변경

--- src/test/java/jslog/post/repository/PostRepositoryTest.java
+++ src/test/java/jslog/post/repository/PostRepositoryTest.java
@@ -1,7 +1,6 @@
 package jslog.post.repository;
 
 import jslog.member.member.domain.Member;
-import jslog.member.member.domain.MemberRole;
 import jslog.member.member.repository.MemberRepository;
 import jslog.post.domain.Post;
 import jslog.post.domain.url.CustomUrl;
@@ -9,7 +8,6 @@ import jslog.postWithTag.domain.PostWithTag;
 import jslog.postWithTag.repository.PostWithTagRepository;
 import jslog.tag.domain.Tag;
 import jslog.tag.repository.TagRepository;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,30 +33,20 @@ class PostRepositoryTest {
     @Autowired TagRepository tagRepository;
     @Autowired PostWithTagRepository postWithTagRepository;
 
-    private final Member testMember1 = Member.create(null, "tester1", MemberRole.MEMBER);
-    private final Member testMember2 = Member.create(null, "tester2", MemberRole.MEMBER);
-
-    @BeforeEach
-    void init() {
-        memberRepository.save(testMember1);
-        memberRepository.save(testMember2);
-    }
-
     @Test
     @DisplayName("게시글 작가 ID와 게시글의 url로 게시글 조회")
     void findByAuthorIdAndCustomUrlUrl() {
         //given
-        Member testMember1 = Member.create(null, "tester1", MemberRole.MEMBER);
-        memberRepository.save(testMember1);
+        Member testMember = Member.create(null, null, null);
+        memberRepository.save(testMember);
 
-        Post post = Post.builder().author(testMember1)
+        Post post = Post.builder().author(testMember)
                 .customUrl(CustomUrl.create("testUrl"))
-                .title("this is test title")
                 .build();
         postRepository.save(post);
 
         //when
-        Post findPost = postRepository.findByAuthorIdAndCustomUrlUrl(testMember1.getId(), post.getStringUrl())
+        Post findPost = postRepository.findByAuthorIdAndCustomUrlUrl(testMember.getId(), post.getStringUrl())
                 .orElseThrow(NoSuchElementException::new);
 
         //then
@@ -69,54 +57,56 @@ class PostRepositoryTest {
     @DisplayName("특정 작가의 게시글을 최신순으로 정렬하여 페이징한 결과를 조회")
     void findByAuthorIdOrderByCreatedDateDescTest() {
         //given
-        Post post1 = Post.builder().author(testMember1).customUrl(CustomUrl.create("1")).build();
-        Post post2 = Post.builder().author(testMember1).customUrl(CustomUrl.create("2")).build();
-        Post post3 = Post.builder().author(testMember1).customUrl(CustomUrl.create("3")).build();
-        Post post4 = Post.builder().author(testMember2).customUrl(CustomUrl.create("4")).build();
+        Member testMember = Member.create(null, null, null);
+        memberRepository.save(testMember);
+
+        Post post1 = Post.builder().author(testMember).build();
+        Post post2 = Post.builder().author(testMember).build();
+        Post post3 = Post.builder().author(testMember).build();
         postRepository.save(post1);
         postRepository.save(post2);
         postRepository.save(post3);
-        postRepository.save(post4);
         List<Post> posts = new ArrayList<>(Arrays.asList(post3,post2,post1));
 
         //when
-        Page<Post> page = postRepository.findByAuthorIdOrderByCreatedDateDesc(testMember1.getId(),
+        Page<Post> page = postRepository.findByAuthorIdOrderByCreatedDateDesc(testMember.getId(),
                 PageRequest.of(0, 5));
 
         //then
-        pagingTest(posts, page);
+        assertThat(page).contains(post3,post2,post1);
     }
 
     @Test
     @DisplayName("특정 작가의 게시글을 최신순으로 정렬하여 슬라이싱")
     void findSliceByAuthorIdOrderByCreatedDateDescTest() {
         //given
-        Post post1 = Post.builder().author(testMember1).customUrl(CustomUrl.create("1")).build();
-        Post post2 = Post.builder().author(testMember1).customUrl(CustomUrl.create("2")).build();
-        Post post3 = Post.builder().author(testMember1).customUrl(CustomUrl.create("3")).build();
-        Post post4 = Post.builder().author(testMember2).customUrl(CustomUrl.create("4")).build();
+        Member testMember = Member.create(null, null, null);
+        memberRepository.save(testMember);
+
+        Post post1 = Post.builder().author(testMember).build();
+        Post post2 = Post.builder().author(testMember).build();
+        Post post3 = Post.builder().author(testMember).build();
         postRepository.save(post1);
         postRepository.save(post2);
         postRepository.save(post3);
-        postRepository.save(post4);
         List<Post> posts = new ArrayList<>(Arrays.asList(post3,post2,post1));
 
         //when
-        Slice<Post> slice = postRepository.findSliceByAuthorIdOrderByCreatedDateDesc(testMember1.getId(),
+        Slice<Post> slice = postRepository.findSliceByAuthorIdOrderByCreatedDateDesc(testMember.getId(),
                 PageRequest.of(0, 5));
 
         //then
-        pagingTest(posts, slice);
+        assertThat(slice).contains(post3,post2,post1);
     }
 
     @Test
     @DisplayName("게시글 제목으로 조회한 결과를 최신순으로 정렬하여 페이징")
     void findByTitleContainingOrderByCreatedDateDescTest() {
         //given
-        Post post1 = Post.builder().author(testMember1).title("테스트페이지").build();
-        Post post2 = Post.builder().author(testMember1).title("테스트 페이지").build();
-        Post post3 = Post.builder().author(testMember1).title("테스 트페이지").build();
-        Post post4 = Post.builder().author(testMember1).title("테 스트페 이지").build();
+        Post post1 = Post.builder().title("테스트페이지").build();
+        Post post2 = Post.builder().title("테스트 페이지").build();
+        Post post3 = Post.builder().title("테스 트페이지").build();
+        Post post4 = Post.builder().title("테 스트페 이지").build();
         postRepository.save(post1);
         postRepository.save(post2);
         postRepository.save(post3);
@@ -129,78 +119,62 @@ class PostRepositoryTest {
         //then
         assertThat(page.getTotalPages()).isEqualTo(1);
         assertThat(page.getTotalElements()).isEqualTo(3);
-
-        pagingTest(posts, page);
+        assertThat(page).contains(post4,post3,post1);
     }
 
     @Test
     @DisplayName("작가 ID와 게시글 제목으로 조회한 결과를 최신순으로 정렬하여 페이징")
     void findByAuthorIdAndTitleContainingOrderByCreatedDateDescTest() {
         //given
-        Post post1 = Post.builder().author(testMember1).title("테스트페이지").build();
-        Post post2 = Post.builder().author(testMember1).title("테스트 페이지").build();
-        Post post3 = Post.builder().author(testMember1).title("테스 트페이지").build();
-        Post post4 = Post.builder().author(testMember2).title("테 스트페 이지").build();
+        Member testMember = Member.create(null, null, null);
+        memberRepository.save(testMember);
+
+        Post post1 = Post.builder().author(testMember).title("테스트페이지").build();
+        Post post2 = Post.builder().author(testMember).title("테스트 페이지").build();
+        Post post3 = Post.builder().author(testMember).title("테스 트페이지").build();
+        Post post4 = Post.builder().author(testMember).title("테 스트페 이지").build();
         postRepository.save(post1);
         postRepository.save(post2);
         postRepository.save(post3);
         postRepository.save(post4);
-        List<Post> posts = new ArrayList<>(Arrays.asList(post3,post1));
+        List<Post> posts = new ArrayList<>(Arrays.asList(post4,post3,post1));
 
         //when
-        Page<Post> page = postRepository.findByAuthorIdAndTitleContainingOrderByCreatedDateDesc(testMember1.getId(),
+        Page<Post> page = postRepository.findByAuthorIdAndTitleContainingOrderByCreatedDateDesc(testMember.getId(),
                 "트페",
                 PageRequest.of(0, 5));
 
         //then
-        pagingTest(posts, page);
+        assertThat(page).contains(post4,post3,post1);
     }
 
     @Test
     @DisplayName("태그 이름과 작가 아이디로 조회한 결과를 최신순으로 정렬하여 페이징")
     void searchByTagAndAuthorIdOrderByCreatedDateDescTest() {
         //given
-        Post post1 = Post.builder().author(testMember1).customUrl(CustomUrl.create("1")).build();
-        Post post2 = Post.builder().author(testMember1).customUrl(CustomUrl.create("2")).build();
-        Post post3 = Post.builder().author(testMember1).customUrl(CustomUrl.create("3")).build();
-        Post post4 = Post.builder().author(testMember2).customUrl(CustomUrl.create("4")).build();
+        Member testMember = Member.create(null, null, null);
+        memberRepository.save(testMember);
+
+        Post post1 = Post.builder().author(testMember).build();
+        Post post2 = Post.builder().author(testMember).build();
         postRepository.save(post1);
         postRepository.save(post2);
-        postRepository.save(post3);
-        postRepository.save(post4);
 
-        String tagName1 = "testTag1";
-        String tagName2 = "testTag2";
-        Tag tag1 = new Tag(tagName1);
-        Tag tag2 = new Tag(tagName2);
+        Tag tag1 = new Tag("testTag1");
         tagRepository.save(tag1);
-        tagRepository.save(tag2);
 
         PostWithTag postWithTagWithTag1 = new PostWithTag(post1, tag1);
-        PostWithTag postWithTagWithTag2 = new PostWithTag(post2, tag2);
-        PostWithTag postWithTagWithTag3 = new PostWithTag(post3, tag2);
-        PostWithTag postWithTagWithTag4 = new PostWithTag(post4, tag2);
+        PostWithTag postWithTagWithTag2 = new PostWithTag(post2, tag1);
         postWithTagRepository.save(postWithTagWithTag1);
         postWithTagRepository.save(postWithTagWithTag2);
-        postWithTagRepository.save(postWithTagWithTag3);
-        postWithTagRepository.save(postWithTagWithTag4);
 
-        List<Post> posts = new ArrayList<>(Arrays.asList(post3,post2));
+        List<Post> posts = new ArrayList<>(Arrays.asList(post2,post1));
         //when
         Page<Post> page = postRepository
-                .findByAuthorIdAndPostWithTagsTagNameOrderByCreatedDateDesc(testMember1.getId(), tagName2, PageRequest.of(0, 3));
+                .findByAuthorIdAndPostWithTagsTagNameOrderByCreatedDateDesc(testMember.getId(), "testTag1", PageRequest.of(0, 5));
 
         //then
-        pagingTest(posts, page);
-    }
-
-    private void pagingTest(List<Post> posts, Slice<Post> page) {
-        int idx = 0, cnt = 0;
-        for(Post post : page) {
-            assertThat(post).isEqualTo(posts.get(idx++));
-            cnt++;
-        }
-        assertThat(cnt).isEqualTo(posts.size());
+        assertThat(page).contains(post2,post1);
     }
 
 }
